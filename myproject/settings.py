97d787851df0df596bac634703f5da42f161c182
staxx6 <staxx@hotmail.de>
2018-01-30 20:39:35 +0100

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'staxx6',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
